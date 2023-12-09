e9db4f1ccfed420b8ca78710e30d6b067ddb2edc
masamichi.sato <masamichi.sato@FVFHQ6JEQ05N.local>
2023-12-09 14:59:41 +0900

    add jira-token tool

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # godzilla_stroy_point
+
+## requirements
+- python
+- rye
+
+## usage
+
+### setup
+
+#### 1. Jira auth
+
+- get jira token from [here](https://id.atlassian.com/manage-profile/security/api-tokens).
+- write it in `./secrets/jira-token`
+- write your Jira email address in `./secrets/jira-email`
+- run `make jira_token`
---
+++ tools/generate_authorization.py
@@ -0,0 +1,33 @@
+import base64
+
+EMAIL_FILE = "./secrets/jira-email"
+TOKEN_FILE = "./secrets/jira-token"
+AUTH_FILE = "./secrets/jira-auth-token.py"
+
+# get email
+try:
+    with open(EMAIL_FILE, "r") as f:
+        line = f.read()
+        email = line.strip()
+except FileNotFoundError:
+    print(f"require Jira user email in {EMAIL_FILE}")
+    exit()
+
+# get token
+try:
+    with open(TOKEN_FILE, "r") as f:
+        line = f.read()
+        token = line.strip()
+except FileNotFoundError:
+    print(f"require Jira token in {TOKEN_FILE}")
+    print("https://id.atlassian.com/manage-profile/security/api-tokens")
+    exit()
+
+# encode base64
+b = f"{email}:{token}".encode()
+b64 = base64.b64encode(b)
+s64 = b64.decode()
+
+# output authorization file
+with open(AUTH_FILE, "w") as f:
+    f.write(f"token = {s64}")
